b53e6eea73b0d1c315a658b50c3baec5b652acff
3SUPERMAX3 <101880494+3SUPERMAX3@users.noreply.github.com>
2023-06-09 16:23:18 +0200

    Add files via upload

---
+++ ćwiczenie 2 koło.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double w, h;
+
+        w = inputValue();
+        h = inputValue();
+        double bMI;
+
+        bMI = w / Math.Pow(h, 2);
+        Console.WriteLine(bMI);
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
