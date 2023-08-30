cde5a3a9dc7c582b31a235a64703f0ac60496665
3SUPERMAX3 <101880494+3SUPERMAX3@users.noreply.github.com>
2023-08-30 16:18:48 +0200

    Add files via upload

---
+++ cwiczenie5.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Wprowadź dwie liczby a i b: ");
+
+Console.Write("a = ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("b = ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+
+if (a == b)
+{
+    Console.WriteLine("Liczby są takie równe");
+}
+else
+{
+    Console.WriteLine($"Większa liczba to {Math.Max(a, b)}");
+}
