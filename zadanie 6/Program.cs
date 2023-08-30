85c00e876be45ad81f46abcb964330e744826697
3SUPERMAX3 <101880494+3SUPERMAX3@users.noreply.github.com>
2023-08-30 16:28:34 +0200

    Add files via upload

---
+++ zadanie 6/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Wprowadź długości boków trójkąta - a, b i c");
+
+Console.Write("a = ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("b = ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("c = ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+if (a == b && b == c)
+{
+    Console.WriteLine("Z tych boków powstanie trójkąt równoboczny");
+}
+else
+    Console.WriteLine("Z tych boków nie powstanie trójkąt równoboczny ani równoramienny");
