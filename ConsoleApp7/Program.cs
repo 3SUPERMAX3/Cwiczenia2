8da7f349d5cbe6edec34e589de8ac911d84c6ca4
3SUPERMAX3 <101880494+3SUPERMAX3@users.noreply.github.com>
2023-08-30 16:32:52 +0200

    Add files via upload

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write($"Podaj x punktu: ");
+double x = double.Parse(Console.ReadLine());
+Console.Write($"Podaj y punktu: ");
+double y = double.Parse(Console.ReadLine());
+Console.Write($"Podaj r: ");
+double r = double.Parse(Console.ReadLine());
+
+double a = 0;
+double b = 0;
+
+
+double pierwszyNawias = x - a;
+double drugiNawias = y - b;
+double lewaStrona = Math.Pow(pierwszyNawias, 2) + Math.Pow(drugiNawias, 2);
+double prawaStrona = Math.Pow(r, 2);
+
+if (lewaStrona <= prawaStrona)
+{
+    Console.WriteLine("Punkt leży w obrębie koła");
+}
+else
+{
+    Console.WriteLine("Punkt jest poza obrębem koła");
+}
